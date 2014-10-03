e66244d6a437d91bd06ff802d705e72c1165e941
Florent PEYSSON <florent.peysson@gmail.com>
2014-10-03 17:04:02 +0200

    Création du TP2 - Calculatrice

---
+++ TAC/OTI/TP2 - Calculatrice/src/money.js
@@ -0,0 +1,31 @@
+
+
+var money = function (v, curr) {
+    curr.trim();
+    this.v = v;
+    this.curr = curr;
+    if (curr.length != 3)
+        throw new DeviseFormatExc(this);
+    if (v < 0)
+        throw new ValeurNegativeExc(this);
+}
+
+money.prototype.getCurrency =
+        function () {
+            return this.curr;
+        };
+
+money.prototype.getValue =
+        function () {
+            return this.v;
+        };
+
+money.prototype.equals =
+        function (otherM) {
+            return (otherM.getValue() == this.getValue() && otherM.getCurrency().toLowerCase() == this.getCurrency().toLowerCase());
+        };
+
+money.prototype.toString =
+        function () {
+            return this.getValue() + " (" + this.getCurrency() + ")";
+        };
---
+++ TAC/OTI/TP2 - Calculatrice/src/money_exc.js
@@ -0,0 +1,35 @@
+function DevisesIncompatibleExc(_d1, _d2) {
+    this.d1 = _d1;
+    this.d2 = _d2;
+
+}
+
+DevisesIncompatibleExc.prototype.toString = function () {
+    return "Devises incompatibles : " + d1 + " vs " + d2;
+}
+
+
+function ValeurNegativeExc(i) {
+    this._i = i;
+}
+
+ValeurNegativeExc.prototype.toString = function () {
+    return "Valeur négative : " + this._i;
+};
+
+
+function DeviseFormatExc(i) {
+    this._i = i;
+}
+
+DeviseFormatExc.prototype.toString = function () {
+    return "Format de devise invalide : " + this._i;
+};
+
+function SoustractionNegativeExc(_d1) {
+    this.d1 = _d1;
+}
+
+SoustractionNegativeExc.prototype.toString = function () {
+    return "Soustraction négative : " + d1;
+}
