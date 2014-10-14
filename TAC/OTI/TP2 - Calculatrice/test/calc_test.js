83c1deada123d6150bc3d382c540e83c92791930
Florent PEYSSON <florent.peysson@gmail.com>
2014-10-15 00:01:56 +0200

    Correction des tests concernant la devise EU au lieu de EUR.

--- TAC/OTI/TP2 - Calculatrice/test/calc_test.js
+++ TAC/OTI/TP2 - Calculatrice/test/calc_test.js
@@ -9,9 +9,9 @@ test("test_computeresults", 1, function ()
     fixture += ("<div id='res'></div>");
     fixture += ("<form id='form0'>");
     fixture += ("<input type='text' id='v1' name='v1' value='2'/>");
-    fixture += ("<input type='text' id='c1' name='c1' value='EU'/>");
+    fixture += ("<input type='text' id='c1' name='c1' value='EUR'/>");
     fixture += ("<input type='text' id='v2' name='v2' value='2'/>");
-    fixture += ("<input type='text' id='c2' name='c2' value='EU'/>");
+    fixture += ("<input type='text' id='c2' name='c2' value='EUR'/>");
     fixture += ("<input type='text' id='ops' name='ops' value='ADD'/>");
     fixture += ("</form>");
 
@@ -20,7 +20,7 @@ test("test_computeresults", 1, function ()
 
     var c = new calc();
     c.computeResult(document.getElementById('form0'));
-    equal(c.message, "Result : 4 (EU)");
+    equal(c.message, "Result : 4 (EUR)");
 }
 );
 
@@ -31,9 +31,9 @@ test("test_computeresults_other", 1, function ()
     fixture += ("<div id='res'></div>");
     fixture += ("<form id='form0'>");
     fixture += ("<input type='text' id='v1' name='v1' value='2'/>");
-    fixture += ("<input type='text' id='c1' name='c1' value='EU'/>");
+    fixture += ("<input type='text' id='c1' name='c1' value='EUR'/>");
     fixture += ("<input type='text' id='v2' name='v2' value='2'/>");
-    fixture += ("<input type='text' id='c2' name='c2' value='EU'/>");
+    fixture += ("<input type='text' id='c2' name='c2' value='EUR'/>");
     fixture += ("<input type='text' id='ops' name='ops' value='SUB'/>");
     fixture += ("</form>");
 
@@ -42,7 +42,7 @@ test("test_computeresults_other", 1, function ()
 
     var c = new calc();
     c.computeResult(document.getElementById('form0'));
-    equal(c.message, "Result : 0 (EU)");
+    equal(c.message, "Result : 0 (EUR)");
     //equal(c.message, "Unsupported operation SUB");
 }
 );
@@ -53,9 +53,9 @@ test("test_displayResult", 1, function ()
     fixture += ("<div id='res'></div>");
     fixture += ("<form id='form0'>");
     fixture += ("<input type='text' id='v1' name='v1' value='2'/>");
-    fixture += ("<input type='text' id='c1' name='c1' value='EU'/>");
+    fixture += ("<input type='text' id='c1' name='c1' value='EUR'/>");
     fixture += ("<input type='text' id='v2' name='v2' value='2'/>");
-    fixture += ("<input type='text' id='c2' name='c2' value='EU'/>");
+    fixture += ("<input type='text' id='c2' name='c2' value='EUR'/>");
     fixture += ("<input type='text' id='ops' name='ops' value='ADD'/>");
     fixture += ("</form>");
 
@@ -66,7 +66,7 @@ test("test_displayResult", 1, function ()
     //c.message = "Result : 4 (EU)";
     //c.displayResult(document.getElementById('res'));
     c.computeResult(document.getElementById('form0'));
-    equal(document.getElementById('res').innerHTML, "Result : 4 (EU)");
+    equal(document.getElementById('res').innerHTML, "Result : 4 (EUR)");
 }
 );
 
